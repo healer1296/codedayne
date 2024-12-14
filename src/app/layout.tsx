a4e6da7f2c7e81e0c537091a2fb6aa4d81dfbdce
Kien Duong <kienduong1296@gmail.com>
2024-12-14 18:00:15 +0700

    add monetag

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,22 +1,22 @@
-import type { Metadata } from "next";
-import localFont from "next/font/local";
-import "./globals.css";
-import Script from "next/script";
+import type { Metadata } from 'next';
+import localFont from 'next/font/local';
+import './globals.css';
+import Script from 'next/script';
 
 const geistSans = localFont({
-  src: "./fonts/GeistVF.woff",
-  variable: "--font-geist-sans",
-  weight: "100 900",
+  src: './fonts/GeistVF.woff',
+  variable: '--font-geist-sans',
+  weight: '100 900',
 });
 const geistMono = localFont({
-  src: "./fonts/GeistMonoVF.woff",
-  variable: "--font-geist-mono",
-  weight: "100 900",
+  src: './fonts/GeistMonoVF.woff',
+  variable: '--font-geist-mono',
+  weight: '100 900',
 });
 
 export const metadata: Metadata = {
-  title: "Get Code",
-  description: "Get code",
+  title: 'Get Code',
+  description: 'Get code',
 };
 
 export default function RootLayout({
@@ -26,6 +26,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        <meta name="monetag" content="28e3540d2d82a4f50cd4d47971d6ff37"></meta>
+      </head>
       <Script
         async
         src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-2183077736845661"
