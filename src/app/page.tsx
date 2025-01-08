70d00c7a2b0aa0a2fb2a685ae3f9d9d9e7e160df
Kien Duong <kienduong1296@gmail.com>
2025-01-08 20:51:30 +0700

    aaa

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -32,7 +32,7 @@ function GetCode() {
   }, []);
 
   const showCode = () => {
-    window.open('https://whomeenoaglauns.com/4/8640111', 'blank');
+    window.open('https://poawooptugroo.com/4/8640111', 'blank');
 
     setLoadingCode(true);
 
@@ -48,7 +48,7 @@ function GetCode() {
       return;
     }
 
-    window.open('https://whomeenoaglauns.com/4/8640111', 'blank');
+    window.open('https://poawooptugroo.com/4/8640111', 'blank');
     setLoadingLink(true);
 
     setTimeout(() => {
