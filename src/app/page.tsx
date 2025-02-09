bb5ec35245af1c0f08b1986df1fcc22585ea5906
Kien Duong <kienduong1296@gmail.com>
2025-02-09 20:58:33 +0700

    av

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -67,88 +67,91 @@ function GetCode() {
           We need funding to maintain the website, so there will be some
           advertisements. We kindly ask for your understanding and support.
         </h2>
-        <h1 className="text-2xl font-semibold flex items-center justify-center gap-4 text-white">
-          Get Code Here
-        </h1>
+        <div className="flex flex-col md:flex-row justify-center gap-4">
+          <div className='md:w-1/3'>
+            <h1 className="mb-4 text-2xl font-semibold flex items-center justify-center gap-4 text-white">
+              Get Code Here
+            </h1>
 
-        {isLoading && (
-          <div className="w-full flex justify-center">
-            <Loading />
-          </div>
-        )}
-
-        {!isLoading && (
-          <div className="flex flex-col gap-6">
-            {displayCode && (
-              <div className="w-full h-12 bg-[#5b93eb] text-white font-semibold text-lg rounded-md flex justify-center items-center">
-                {(!loadingCode && code?.code) || 'No code'}
+            {isLoading && (
+              <div className="w-full flex justify-center">
+                <Loading />
               </div>
             )}
 
-            {!displayCode && (
-              <button
-                className="w-full h-12 bg-[#5b93eb] text-white font-semibold text-lg rounded-md text-center   "
-                onClick={showCode}
-              >
-                {loadingCode && (
-                  <div className="px-3 py-1 text-xs w-full h-12 font-medium leading-none text-center text-blue-800 bg-blue-200 rounded-md animate-pulse">
-                    <div className="w-ful h-full flex justify-center items-center">
-                      <Loading />
-                    </div>
+            {!isLoading && (
+              <div className="flex flex-col gap-6">
+                {displayCode && (
+                  <div className="w-full h-12 border-white border  text-white font-semibold text-lg rounded-md flex justify-center items-center">
+                    {(!loadingCode && code?.code) || 'No code'}
                   </div>
                 )}
-                {!loadingCode && 'Get Code'}
-              </button>
-            )}
 
-            {displayLink && (
-              <div
-                className="w-full h-12 bg-[#5b93eb] text-white font-semibold text-sm text-center rounded-md flex justify-center items-center cursor-pointer"
-                onClick={() => openLink(code.link)}
-              >
-                {(!loadingLink && code?.link) || 'No link'}
+                {!displayCode && (
+                  <button
+                    className="w-full h-12 border-white border text-white font-semibold text-lg rounded-md text-center   "
+                    onClick={showCode}
+                  >
+                    {loadingCode && (
+                      <div className="px-3 py-1 text-xs w-full h-12 font-medium leading-none text-center text-blue-800 bg-blue-200 rounded-md animate-pulse">
+                        <div className="w-ful h-full flex justify-center items-center">
+                          <Loading />
+                        </div>
+                      </div>
+                    )}
+                    {!loadingCode && 'Get Code'}
+                  </button>
+                )}
+
+                {displayLink && (
+                  <div
+                    className="w-full h-12 border-white border text-white font-semibold text-sm text-center rounded-md flex justify-center items-center cursor-pointer"
+                    onClick={() => openLink(code.link)}
+                  >
+                    {(!loadingLink && code?.link) || 'No link'}
+                  </div>
+                )}
+
+                {!displayLink && (
+                  <button
+                    className="w-full h-12 border-white border text-white font-semibold text-lg rounded-md text-center   "
+                    onClick={showLink}
+                  >
+                    {loadingLink && (
+                      <div className="px-3 py-1 text-xs w-full h-12 font-medium leading-none text-center text-blue-800 bg-blue-200 rounded-md animate-pulse">
+                        <div className="w-ful h-full flex justify-center items-center">
+                          <Loading />
+                        </div>
+                      </div>
+                    )}
+                    {!loadingLink && (
+                      <div className="flex justify-center items-center gap-4">
+                        Get Link
+                      </div>
+                    )}
+                  </button>
+                )}
+
+                <a href="https://x.com/xuangiang497759" target="blank">
+                  <button className="w-full h-12 border-white border text-white font-semibold text-lg rounded-md">
+                    See More
+                  </button>
+                </a>
               </div>
             )}
-
-            {!displayLink && (
-              <button
-                className="w-full h-12 bg-[#5b93eb] text-white font-semibold text-lg rounded-md text-center   "
-                onClick={showLink}
-              >
-                {loadingLink && (
-                  <div className="px-3 py-1 text-xs w-full h-12 font-medium leading-none text-center text-blue-800 bg-blue-200 rounded-md animate-pulse">
-                    <div className="w-ful h-full flex justify-center items-center">
-                      <Loading />
-                    </div>
-                  </div>
-                )}
-                {!loadingLink && (
-                  <div className="flex justify-center items-center gap-4">
-                    Get Link
-                  </div>
-                )}
-              </button>
-            )}
-
-            <a href="https://x.com/TrungKinDng2" target="blank">
-              <button className="w-full h-12 bg-[#5b93eb] text-white font-semibold text-lg rounded-md">
-                See More
-              </button>
-            </a>
           </div>
-        )}
-      </div>
-
-      <div className="w-full border-t border-white/50 grid grid-cols-2 md:grid-cols-4 lg:grid-cols-7 gap-4 py-4 px-4">
-        {list.map((item) => (
-          <button
-            className="bg-white py-4 font-bold"
-            key={item.id}
-            onClick={() => window.open(item.link, 'blank')}
-          >
-            <span>{item.code}</span>
-          </button>
-        ))}
+          <div className="w-full grid grid-cols-2 md:grid-cols-4 lg:grid-cols-7 gap-4 py-4 px-4">
+            {list.map((item) => (
+              <button
+                className="bg-white py-4 font-bold rounded-md text-sm text-black"
+                key={item.id}
+                onClick={() => window.open(item.link, 'blank')}
+              >
+                <span>{item.code}</span>
+              </button>
+            ))}
+          </div>
+        </div>
       </div>
     </div>
   );
